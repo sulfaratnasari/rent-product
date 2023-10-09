91433ed1bf9860d8c4f793c45790ff30f61e12d2
Sulfa Ratna Sari <sulfaratnasari@gmail.com>
2023-10-10 01:11:34 +0700

    Update init.sql

--- initdb/init.sql
+++ initdb/init.sql
@@ -30,3 +30,17 @@ INSERT INTO rp_order (stock_item_id, start_date, end_date) VALUES (2, '02-10-202
 INSERT INTO rp_order (stock_item_id, start_date, end_date) VALUES (3, '02-10-2023', '10-10-2023');
 INSERT INTO rp_order (stock_item_id, start_date, end_date) VALUES (4, '02-10-2023', '10-10-2023');
 INSERT INTO rp_order (stock_item_id, start_date, end_date) VALUES (2, '12-10-2023', '15-10-2023');
+
+
+INSERT INTO rp_product  (name, description) VALUES ('Monitor B', 'This is Monitor B');
+
+INSERT INTO rp_stock_item  (product_id) VALUES (2);
+INSERT INTO rp_stock_item  (product_id) VALUES (2);
+INSERT INTO rp_stock_item  (product_id) VALUES (2);
+INSERT INTO rp_stock_item  (product_id) VALUES (2);
+
+INSERT INTO rp_order (stock_item_id, start_date, end_date) VALUES (5, '02-11-2023', '10-11-2023');
+INSERT INTO rp_order (stock_item_id, start_date, end_date) VALUES (6, '02-11-2023', '10-11-2023');
+INSERT INTO rp_order (stock_item_id, start_date, end_date) VALUES (7, '02-11-2023', '10-11-2023');
+INSERT INTO rp_order (stock_item_id, start_date, end_date) VALUES (8, '02-11-2023', '10-11-2023');
+INSERT INTO rp_order (stock_item_id, start_date, end_date) VALUES (6, '12-11-2023', '15-11-2023');
